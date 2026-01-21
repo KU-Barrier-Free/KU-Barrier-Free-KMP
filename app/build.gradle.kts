1797f9777e106047309b5634921ea30e7f8cde92
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2026-01-21 18:40:58 +0900

    chore: 앱 버전 네임 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "com.ganalijigi.kubf"
         minSdk = 30
         targetSdk = 35
-        versionCode = 12
-        versionName = "1.0.11"
+        versionCode = 13
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
