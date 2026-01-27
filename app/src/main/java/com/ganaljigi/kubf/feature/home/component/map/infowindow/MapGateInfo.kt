b263d3c12fb8d34ba2fe2d911762ece03c53c488
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2026-01-28 04:08:43 +0900

    chore: MapGateInfo.kt 프리뷰 코드 제거 #15

--- app/src/main/java/com/ganaljigi/kubf/feature/home/component/map/infowindow/MapGateInfo.kt
+++ app/src/main/java/com/ganaljigi/kubf/feature/home/component/map/infowindow/MapGateInfo.kt
@@ -22,12 +22,9 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.ganalijigi.kubf.R
 import com.ganaljigi.kubf.core.designsystem.theme.Gray2
 import com.ganaljigi.kubf.core.designsystem.theme.KUBFAndroidTheme
 
@@ -77,17 +74,6 @@ fun MapGateInfo(
                 lineHeight = lineHeight.sp,
             ),
             textAlign = TextAlign.Center,
-        )
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-private fun MapGateInfoPreview() {
-    KUBFAndroidTheme {
-        MapGateInfo(
-            description = "정문 출입구입니다.",
-            painters = listOf(painterResource(R.drawable.ic_building_marker)),
         )
     }
 }
