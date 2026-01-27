0a07a981be54bf87534111340622a430c202c112
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2026-01-27 19:07:33 +0900

    chore: 불필요한 AppModule 삭제 #4

--- app/src/main/java/com/ganaljigi/kubf/core/data/di/AppModule.kt
+++
@@ -1,8 +0,0 @@
-package com.ganaljigi.kubf.core.data.di
-
-import org.koin.core.annotation.ComponentScan
-import org.koin.core.annotation.Module
-
-@Module(includes = [NetworkModule::class, ApiModule::class])
-@ComponentScan("com.ganaljigi.kubf")
-class AppModule
