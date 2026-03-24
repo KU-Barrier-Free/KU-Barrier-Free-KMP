6691129933059f760b374496a271a934a41186df
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2026-03-24 20:03:48 +0900

    fix: 테마 배경색을 White로 명시하여 다크모드 대응 #24

--- composeApp/src/commonMain/kotlin/com/ganaljigi/kubf/core/designsystem/theme/Theme.kt
+++ composeApp/src/commonMain/kotlin/com/ganaljigi/kubf/core/designsystem/theme/Theme.kt
@@ -4,21 +4,14 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.graphics.Color
 
 private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
     tertiary = Pink40,
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-     */
+    background = Color.White,
+    surface = Color.White,
 )
 
 @Composable
