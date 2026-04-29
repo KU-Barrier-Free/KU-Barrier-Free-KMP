fb9b5145d78b3405fce6066b9f30ff63b9871ae9
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2026-04-29 21:24:45 +0900

    feat: iOS Firebase Analytics 설정 추가 #24

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,8 +1,21 @@
 import SwiftUI
 import GoogleMaps
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
 
 @main
 struct iOSApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
     init() {
         if let apiKey = Bundle.main.object(forInfoDictionaryKey: "GOOGLE_MAPS_API_KEY") as? String,
            !apiKey.isEmpty, apiKey != "$(GOOGLE_MAPS_API_KEY)" {
