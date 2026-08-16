d571c3f192783c8a67a47803e43dff35e2a26657
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2026-08-16 22:40:56 +0900

    chore: Android 릴리즈 버전 1.1.8로 업데이트

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -148,8 +148,8 @@ android {
         applicationId = "com.ganalijigi.kubf"
         minSdk = 30
         targetSdk = 36
-        versionCode = 20
-        versionName = "1.1.7"
+        versionCode = 21
+        versionName = "1.1.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
